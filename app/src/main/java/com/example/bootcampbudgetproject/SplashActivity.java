83acd84272b25c6f2954ac1e33e5d46c8e0e3d6c
halil9393 <halilibrahimk.93@gmail.com>
2022-04-08 15:03:19 +0300

    ana ekran tasarim yapildi

--- app/src/main/java/com/example/bootcampbudgetproject/SplashActivity.java
+++ app/src/main/java/com/example/bootcampbudgetproject/SplashActivity.java
@@ -2,15 +2,14 @@ package com.example.bootcampbudgetproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 
 
