6fde6d4f51599c14f94a757dbaf88153b0fc4f7a
halil9393 <halilibrahimk.93@gmail.com>
2022-04-10 14:30:41 +0300

    recyclerview bitirildi

--- app/src/main/java/com/example/bootcampbudgetproject/MainActivity.java
+++ app/src/main/java/com/example/bootcampbudgetproject/MainActivity.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bootcampbudgetproject.adapters.BudgetListAdapter;
+import com.example.bootcampbudgetproject.enums.BudgetType;
+import com.example.bootcampbudgetproject.models.MSpend;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvName, tvHarcama,tvMiktar, btnTL, btnDOLAR, btnSTERLIN, btnEURO;
     private RecyclerView recyclerView;
+    private ArrayList<MSpend> spendList;
 
 
     @Override
@@ -19,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
+        initRecyclerView();
 
     }
 
@@ -31,8 +40,30 @@ public class MainActivity extends AppCompatActivity {
         btnSTERLIN = findViewById(R.id.btnSTERLIN);
         btnEURO = findViewById(R.id.btnEURO);
         recyclerView = findViewById(R.id.recyclerView);
+        spendList = new ArrayList<>();
+
+
+        //mock
+        MSpend mSpend = new MSpend("Ev Kirası",1000, BudgetType.Kira);
+        spendList.add(mSpend);
+        mSpend = new MSpend("Elektrik Faturası",500,BudgetType.Fatura);
+        spendList.add(mSpend);
+        mSpend = new MSpend("Ev Aidatı",100,BudgetType.Diger);
+        spendList.add(mSpend);
+        mSpend = new MSpend("Su Faturası",300,BudgetType.Fatura);
+        spendList.add(mSpend);
+
+
     }
 
+    private void initRecyclerView() {
+
+        BudgetListAdapter budgetListAdapter = new BudgetListAdapter(MainActivity.this, spendList);
+        recyclerView.setAdapter(budgetListAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
 
 
 
